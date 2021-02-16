16419b78e12aac420b4e762f222a04b50aa31b18
XodTuza <gordie2u@gmail.com>
2021-02-16 17:37:36 +0100

    fixes

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cors = require('cors');
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 dotenv.config();
 
-
 //Import Routes
-const authRoute = require('./api/routes/auth');
-const profileRoute = require('./api/routes/profile');
-
+const authRoute = require("./api/routes/auth");
+const profileRoute = require("./api/routes/profile");
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { 
-        useNewUrlParser: true, useUnifiedTopology: true }, () => 
-        console.log('db connected')
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("db connected")
 );
-
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
+app.get("/", async (req, res) => {
+  res.send('Open-Auth-api');
+});
+
 //Routes middlewares
-app.use('/api/auth', authRoute);
-app.use('/api/profile', profileRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
 
 app.listen(process.env.PORT || 3000);
